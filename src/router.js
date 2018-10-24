d8a917437aa7055ddf677b026971b5c962b35096
mrfox1 <lsdovscky@gmail.com>
2018-10-24 17:26:12 +0300

    add header and home components

--- src/router.js
+++ src/router.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from './components/Home.vue';
 
 Vue.use(VueRouter)
 
+const routes = [
+    {path: '/', component: Home}
+]
 
 export default new VueRouter({mode: 'history', routes})
