0a62adc4f4cfdc6f86ee6d523eb8dd2a960d9830
mrfox1 <lsdovscky@gmail.com>
2018-10-24 16:03:36 +0300

    init commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
